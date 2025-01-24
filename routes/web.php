9f330f17e5a47fb848fecda3f828b69369e35e50
vindrayulian <vindrayulian@gmail.com>
2025-01-24 09:31:33 +0700

    style : create view homepage

--- routes/web.php
+++ routes/web.php
@@ -5,26 +5,25 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Event\EventController;
 use App\Http\Controllers\Auth\RegisterController;
-
-
+use App\Http\Controllers\Homepage\HomepageController;
 
 // =============== Auth ===============
-
 // ===== Register =====
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/store-register', [RegisterController::class, 'store'])->name('store-register');
-
 // ===== Login =====
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 
 
 
+// =============== HomePage ===============
+Route::get('/', [HomepageController::class, 'index'])->name('homepage');
 
-// =============== Menu ===============
 
+
+// =============== Admin Menu ===============
 // ===== Dashboard =====
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
 // ===== Event =====
 Route::get('/event', [EventController::class, 'index'])->name('event');
 
