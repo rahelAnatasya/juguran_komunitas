d418e91626d13e3b755894d4b7cc5a9d59e0e033
rahelAnatasya <147134769+rahelAnatasya@users.noreply.github.com>
2025-02-13 16:55:22 +0700

    Revisi AOS dan Scrolling

--- app/Http/Controllers/Homepage/HomepageController.php
+++ app/Http/Controllers/Homepage/HomepageController.php
@@ -7,12 +7,84 @@ use Illuminate\Http\Request;
 
 class HomepageController extends Controller
 {
-    public function index(){
+    public function index()
+    {
+        $events = [
+            [
+                'title' => 'Juguran Komunitas - Transform Your Design Skills',
+                'date' => 'Januari 25, 2025',
+                'location' => 'Warung Mulyo, Pabuaran, Purwokerto Utara, Banyumas',
+                'status' => 'Expired',
+                'image' => asset('assets/images/poster-januari-2025.jpg')
+            ],
+            [
+                'title' => 'Juguran Komunitas - Transform Your Design Skills',
+                'date' => 'Januari 25, 2025',
+                'location' => 'Warung Mulyo, Pabuaran, Purwokerto Utara, Banyumas',
+                'status' => 'Expired',
+                'image' => asset('assets/images/poster-januari-2025.jpg')
+            ],
+            [
+                'title' => 'Juguran Komunitas - Transform Your Design Skills',
+                'date' => 'Januari 25, 2025',
+                'location' => 'Warung Mulyo, Pabuaran, Purwokerto Utara, Banyumas',
+                'status' => 'Expired',
+                'image' => asset('assets/images/poster-januari-2025.jpg')
+            ],
+            [
+                'title' => 'Juguran Komunitas - Transform Your Design Skills',
+                'date' => 'Januari 25, 2025',
+                'location' => 'Warung Mulyo, Pabuaran, Purwokerto Utara, Banyumas',
+                'status' => '',
+                'image' => asset('assets/images/poster-januari-2025.jpg')
+            ],
+            [
+                'title' => 'Juguran Komunitas - Transform Your Design Skills',
+                'date' => 'Januari 25, 2025',
+                'location' => 'Warung Mulyo, Pabuaran, Purwokerto Utara, Banyumas',
+                'status' => '',
+                'image' => asset('assets/images/poster-januari-2025.jpg')
+            ],
+            [
+                'title' => 'Juguran Komunitas - Transform Your Design Skills',
+                'date' => 'Januari 25, 2025',
+                'location' => 'Warung Mulyo, Pabuaran, Purwokerto Utara, Banyumas',
+                'status' => '',
+                'image' => asset('assets/images/poster-januari-2025.jpg')
+            ],
+        ];
+
+        $features = [
+            [
+                'icon' => asset('assets/images/icon/connection.svg'),
+                'title' => 'Meningkatkan Koneksi',
+                'description' => 'Perluas Jaringan dan temui orang-orang dengan minat yang sama'
+            ],
+            [
+                'icon' => asset('assets/images/icon/experience.svg'),
+                'title' => 'Berbagi Pengalaman',
+                'description' => 'Bagikan perjalanan dan pelajaran berharga yang bisa menginspirasi.'
+            ],
+            [
+                'icon' => asset('assets/images/icon/insight.svg'),
+                'title' => 'Wawasan Baru',
+                'description' => 'Dapatkan wawasan baru dari sudut pandang yang berbeda dan unik.'
+            ],
+            [
+                'icon' => asset('assets/images/icon/support.svg'),
+                'title' => 'Dukungan Positif',
+                'description' => 'Temukan dukungan dan motivasi untuk berkembang bersama secara positif.'
+            ]
+        ];
+
         return view('homepage.index', [
-            'title' => 'Juguran Komunitas'
+            'title' => 'Juguran Komunitas',
+            'events' => $events,
+            'features' => $features,
         ]);
     }
-    public function detail() {
+    public function detail()
+    {
         return view('homepage.detail');
     }
 }
--- resources/views/homepage/detail.blade.php
+++ resources/views/homepage/detail.blade.php
@@ -1,158 +1,161 @@
 @extends('homepage.components.layout')
 @section('content')
 
-<div class="container py-1">
-  <div class="py-5 mt-3"></div>
-  <div class="overflow-hidden rounded-4 d-flex justify-content-center align-items-center" style="max-height: 580px">
+  <div class="container py-1">
+    <div class="py-5 mt-3"></div>
+    <div data-aos="fade-up" data-aos-duration="1000"
+    class="overflow-hidden rounded-4 d-flex justify-content-center align-items-center" style="max-height: 580px">
     <img class="w-100" src="{{ asset('assets/images/poster-januari-2025.jpg') }}" alt="" />
+    </div>
   </div>
-</div>
 
-<div class="container py-4">
-  <div class="mx-1">
-    <div class="d-flex gap-3 justify-content-between justify-content-sm-start">
+  <div class="container py-4">
+    <div class="mx-1">
+    <div data-aos="fade-right" data-aos-delay="200"
+      class="d-flex gap-3 justify-content-between justify-content-sm-start">
       <button class="card-hover border-0 p-0 overflow-hidden d-flex align-items-center rounded-3 text-white"
-        style="background-color: #434a4d">
-        <img class="p-0" src="{{ asset('assets/images/icon/copy.svg') }}" alt="" />
-        <span class="px-2 px-md-3 font-sm-small">Copy</span>
+      style="background-color: #434a4d">
+      <img class="p-0" src="{{ asset('assets/images/icon/copy.svg') }}" alt="" />
+      <span class="px-2 px-md-3 font-sm-small">Copy</span>
       </button>
       <button class="card-hover border-0 p-0 overflow-hidden d-flex align-items-center rounded-3 text-white"
-        style="background-color: #34af23">
-        <img class="" src="{{ asset('assets/images/icon/whatsapp.svg') }}" alt="" />
-        <span class="px-2 px-md-3 font-sm-small">Whatsapp</span>
+      style="background-color: #34af23">
+      <img class="" src="{{ asset('assets/images/icon/whatsapp.svg') }}" alt="" />
+      <span class="px-2 px-md-3 font-sm-small">Whatsapp</span>
       </button>
       <button class="card-hover border-0 p-0 overflow-hidden d-flex align-items-center rounded-3 text-white"
-        style="background-color: #9d38b6">
-        <img class="" src="{{ asset('assets/images/icon/instagram2.svg') }}" alt="" />
-        <span class="px-2 px-md-3 font-sm-small">Instagram</span>
+      style="background-color: #9d38b6">
+      <img class="" src="{{ asset('assets/images/icon/instagram2.svg') }}" alt="" />
+      <span class="px-2 px-md-3 font-sm-small">Instagram</span>
       </button>
     </div>
-    <h1 class="font-18 font-md-50-800 mt-3">
+    <h1 data-aos="fade-up" data-aos-delay="300" class="font-18 font-md-50-800 mt-3">
       Juguran Komunitas - TRANSFORM YOUR DESIGN SKILLS
     </h1>
-    <div class="d-flex align-items-center gap-3 pt-3">
+    <div data-aos="fade-up" data-aos-delay="400" class="d-flex align-items-center gap-3 pt-3">
       <img class="rounded-pill overflow-hidden" style="max-width: 45px"
-        src="{{ asset('assets/images/users/dummy-avatar.jpg') }}" alt="" />
+      src="{{ asset('assets/images/users/dummy-avatar.jpg') }}" alt="" />
       <div class="font-12-700 font-md-16-700 ">By Juguran Komunitas</div>
     </div>
+    </div>
   </div>
-</div>
 
-<!-- Detail -->
-<div class="container">
-  <div class="px-lg-5 pb-5 px-3 shadow-lg mx-2 card pt-4 rounded-4">
+  <!-- Detail -->
+  <div class="container">
+    <div class="px-lg-5 pb-5 px-3 shadow-lg mx-2 card pt-4 rounded-4">
     <div class="row pb-4">
-      <div class="col-12 col-lg-6">
-        <div class="d-flex h-100 flex-column justify-content-between">
-          <div class="d-flex flex-column gap-4">
-            <div>
-              <h3 class="font-20-700  pb-2">Tanggal dan Waktu</h3>
-              <div class="d-flex align-items-center gap-3 mb-2">
-                <img style="width: 30px; height: 30px" src="{{ asset('assets/images/icon/calendar.svg') }}"
-                  alt="Tanggal" />
-                <span class="font-14-600 font-md-16-600">25 Januari 2025</span>
-              </div>
-              <div class="d-flex align-items-center gap-3">
-                <img style="width: 30px; height: 30px" src="{{ asset('assets/images/icon/clock.svg') }}" alt="Waktu" />
-                <span class="font-14-600 font-md-16-600">13.00 WIB s.d Selesai</span>
-              </div>
-            </div>
-            <div>
-              <h3 class="font-20-700 ">Lokasi Acara</h3>
-              <div class="d-flex align-items-center gap-3">
-                <img style="width: 30px; height: 30px" src="{{ asset('assets/images/icon/location.svg') }}"
-                  alt="Lokasi" />
-                <span class="font-14-600 font-md-16-600">Warung Mulyo, Pabuaran, Purwokerto Utara, Banyumas</span>
-              </div>
-            </div>
+      <div data-aos="fade-right" data-aos-delay="200" class="col-12 col-lg-6">
+      <div class="d-flex h-100 flex-column justify-content-between">
+        <div class="d-flex flex-column gap-4">
+        <div>
+          <h3 class="font-20-700  pb-2">Tanggal dan Waktu</h3>
+          <div class="d-flex align-items-center gap-3 mb-2">
+          <img style="width: 30px; height: 30px" src="{{ asset('assets/images/icon/calendar.svg') }}"
+            alt="Tanggal" />
+          <span class="font-14-600 font-md-16-600">25 Januari 2025</span>
           </div>
-          <a class="button-hover btn bg-primary-dark text-white w-100 fw-bold py-2 my-3" href="">Lihat Detail</a>
+          <div class="d-flex align-items-center gap-3">
+          <img style="width: 30px; height: 30px" src="{{ asset('assets/images/icon/clock.svg') }}" alt="Waktu" />
+          <span class="font-14-600 font-md-16-600">13.00 WIB s.d Selesai</span>
+          </div>
         </div>
+        <div>
+          <h3 class="font-20-700 ">Lokasi Acara</h3>
+          <div class="d-flex align-items-center gap-3">
+          <img style="width: 30px; height: 30px" src="{{ asset('assets/images/icon/location.svg') }}"
+            alt="Lokasi" />
+          <span class="font-14-600 font-md-16-600">Warung Mulyo, Pabuaran, Purwokerto Utara, Banyumas</span>
+          </div>
+        </div>
+        </div>
+        <a class="button-hover btn bg-primary-dark text-white w-100 fw-bold py-2 my-3" href="">Lihat Detail</a>
       </div>
-      <div class="col-12 col-lg-6">
-        <h3 class="font-20-700 py-1 ">Jadwal Acara</h3>
-        <!-- tabel -->
-        <table style="border: var(--color-background-primary)" class="table table-bordered table-hover text-center m-0">
-          <thead class="font-14-600 text-white" style="background-color: var(--color-primary)">
-            <tr>
-              <th scope="col">No</th>
-              <th class="text-start" scope="col">Kegiatan</th>
-              <th scope="col">Waktu</th>
-            </tr>
-          </thead>
-          <tbody class="font-14-500">
-            <tr>
-              <td>1</td>
-              <td class="text-start">Registrasi Peserta</td>
-              <td>13.00 - 13.30</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td class="text-start">Pembukaan</td>
-              <td>13.30 - 14.00</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td class="text-start">Sesi Materi</td>
-              <td>14.00 - 15.30</td>
-            </tr>
-            <tr>
-              <td>4</td>
-              <td class="text-start">Break</td>
-              <td>15.30 - 15.45</td>
-            </tr>
-            <tr>
-              <td>5</td>
-              <td class="text-start">Sesi Praktik</td>
-              <td>15.45 - 17.00</td>
-            </tr>
-          </tbody>
-        </table>
+      </div>
+      <div data-aos="fade-left" data-aos-delay="400" class="col-12 col-lg-6">
+      <!-- tabel -->
+      <table style="border: var(--color-background-primary)" class="table table-bordered table-hover text-center m-0">
+        <thead class="font-14-600 text-white" style="background-color: var(--color-primary)">
+        <tr>
+          <th scope="col">No</th>
+          <th class="text-start" scope="col">Kegiatan</th>
+          <th scope="col">Waktu</th>
+        </tr>
+        </thead>
+        <tbody class="font-14-500">
+        <tr>
+          <td>1</td>
+          <td class="text-start">Registrasi Peserta</td>
+          <td>13.00 - 13.30</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td class="text-start">Pembukaan</td>
+          <td>13.30 - 14.00</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td class="text-start">Sesi Materi</td>
+          <td>14.00 - 15.30</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td class="text-start">Break</td>
+          <td>15.30 - 15.45</td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td class="text-start">Sesi Praktik</td>
+          <td>15.45 - 17.00</td>
+        </tr>
+        </tbody>
+      </table>
       </div>
     </div>
-    <div>
-      <h3 class="font-20-700 pb-3 ">Deskripsi</h3>
+
+    <div data-aos="fade-up" data-aos-delay="200">
+      <h3 class="font-20-700 pb-3">Deskripsi</h3>
       <p class="font-14-500">
-        Transform Your Design Skills Bersiaplah untuk menjelajahi dunia
-        UI/UX yang seru, dinamis, dan penuh tantangan! Event ini dirancang
-        khusus untuk kamu, para creative Gen Z yang ingin meningkatkan
-        kemampuan desain dan memperluas wawasan di bidang teknologi. Dengan
-        materi praktis dan insightful dari para ahli, kamu akan mendapatkan
-        pengalaman belajar yang gratis dan penuh manfaat, cocok untuk pemula
-        maupun yang sudah berpengalaman. Jangan lewatkan kesempatan untuk
-        upgrade skill, berjejaring dengan komunitas kreatif lainnya, dan
-        menjadi bagian dari masa depan desain! Informasi lebih lanjut akan
-        segera diumumkan, jadi stay tuned dan segera daftar!
+      Transform Your Design Skills Bersiaplah untuk menjelajahi dunia
+      UI/UX yang seru, dinamis, dan penuh tantangan! Event ini dirancang
+      khusus untuk kamu, para creative Gen Z yang ingin meningkatkan
+      kemampuan desain dan memperluas wawasan di bidang teknologi. Dengan
+      materi praktis dan insightful dari para ahli, kamu akan mendapatkan
+      pengalaman belajar yang gratis dan penuh manfaat, cocok untuk pemula
+      maupun yang sudah berpengalaman. Jangan lewatkan kesempatan untuk
+      upgrade skill, berjejaring dengan komunitas kreatif lainnya, dan
+      menjadi bagian dari masa depan desain! Informasi lebih lanjut akan
+      segera diumumkan, jadi stay tuned dan segera daftar!
       </p>
     </div>
-    <div>
-      <h3 class="font-20-700 ">Penyelenggara</h3>
+
+    <div data-aos="fade-up" data-aos-offset="200" data-aos-delay="300">
+      <h3 class="font-20-700">Penyelenggara</h3>
       <div class="d-flex flex-wrap justify-content-center gap-4 px-5 pt-3 pb-5">
-        <img style="width: 250px" src="{{ asset('assets/images/logo/icon-juguran-full.png') }}"
-          alt="Icon Pengelenggara" />
+      <img data-aos="zoom-in" data-aos-delay="400" style="width: 250px"
+        src="{{ asset('assets/images/logo/icon-juguran-full.png') }}" alt="Icon Pengelenggara" />
       </div>
     </div>
-    <div>
-      <h3 class="font-20-700 ">Didukung Oleh</h3>
+
+    <div data-aos="fade-up" data-aos-offset="200" data-aos-offset="200" data-aos-delay="400">
+      <h3 class="font-20-700">Didukung Oleh</h3>
       <div class="d-flex flex-wrap justify-content-center gap-4 pt-3 pb-5">
-        <img class="partner-logo" src="{{ asset('assets/images/logo/logo-ipsum.svg') }}" alt="" />
-        <img class="partner-logo" src="{{ asset('assets/images/logo/logo-ipsum.svg') }}" alt="" />
-        <img class="partner-logo" src="{{ asset('assets/images/logo/logo-ipsum.svg') }}" alt="" />
-        <img class="partner-logo" src="{{ asset('assets/images/logo/logo-ipsum.svg') }}" alt="" />
-        <img class="partner-logo" src="{{ asset('assets/images/logo/logo-ipsum.svg') }}" alt="" />
+      @for($i = 1; $i <= 5; $i++)
+      <img data-aos="zoom-in" data-aos-delay="{{ 400 + ($i * 100) }}" class="partner-logo"
+      src="{{ asset('assets/images/logo/logo-ipsum.svg') }}" alt="" />
+    @endfor
       </div>
     </div>
-    <div>
-      <h3 class="font-20-700 ">Media Partner</h3>
+
+    <div data-aos="fade-up" data-aos-offset="200" data-aos-offset="200" data-aos-delay="500">
+      <h3 class="font-20-700">Media Partner</h3>
       <div class="d-flex flex-wrap justify-content-center gap-4 pt-3">
-        <img class="partner-logo" src="{{ asset('assets/images/logo/logo-ipsum.svg') }}" alt="" />
-        <img class="partner-logo" src="{{ asset('assets/images/logo/logo-ipsum.svg') }}" alt="" />
-        <img class="partner-logo" src="{{ asset('assets/images/logo/logo-ipsum.svg') }}" alt="" />
-        <img class="partner-logo" src="{{ asset('assets/images/logo/logo-ipsum.svg') }}" alt="" />
-        <img class="partner-logo" src="{{ asset('assets/images/logo/logo-ipsum.svg') }}" alt="" />
+      @for($i = 1; $i <= 5; $i++)
+      <img data-aos="zoom-in" data-aos-delay="{{ 500 + ($i * 100) }}" class="partner-logo"
+      src="{{ asset('assets/images/logo/logo-ipsum.svg') }}" alt="" />
+    @endfor
       </div>
+    </div>
     </div>
   </div>
-</div>
 
 @endsection
